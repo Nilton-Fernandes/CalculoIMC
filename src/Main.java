c4831e61adf5842265161848ba31fe4a638d0ec5
Nilton Fernandes <107441723+Nilton-Fernandes@users.noreply.github.com>
2024-08-07 07:43:07 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -30,15 +30,6 @@ public class Main {
 
             }
             PesoIdeal.calculoDoPesoIdeal(altura,imc);
-//
-//        if (imc <18.5 || imc >24.5) {
-//            double pesoIdealMin = altura * altura * 18.5;
-//            double pesoIdealMax = altura * altura * 24.9;
-//
-//            System.out.printf("\nPESO IDEAL MÍNIMO = %.2f\nPESO IDEAL MÁXIMO = %.2f", pesoIdealMin, pesoIdealMax);
-//        }
-
-
 
     }
 
